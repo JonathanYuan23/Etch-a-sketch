61bab8618661751c2ad35b6df7fcc18ca5b3a1fc
Jonathan Yuan <jy90023@gmail.com>
2021-07-29 19:34:20 -0400

    added buttons to control the options for the sketch pad

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -1,22 +1,26 @@
 const sketchPad = document.querySelector('#sketch-pad');
-let mouseDown = 0;
 
-function initialize(width) {
+let width = 16;
+let color = 'rgb(50, 50, 50)';
+let click = 0;
+
+function initialize() {
     for (let row = 1; row <= width; row++) {
         for (let col = 1; col <= width; col++) {
             const item = document.createElement('div');
             item.style.gridRow = `${row}`;
             item.style.gridColumn = `${col}`;
-            item.addEventListener('mousedown', () => {
-                item.style.backgroundColor = 'rgb(50, 50, 50)';
-                mouseDown = !mouseDown;
+            item.classList.add('100');
+            item.addEventListener('click', () => {
+                item.style.backgroundColor = color;
+                click = !click;
             });
             item.addEventListener('mouseover', () => {
-                if (mouseDown) item.style.backgroundColor = 'rgb(50, 50, 50)';
+                if (click) item.style.backgroundColor = color;
             });
             sketchPad.appendChild(item);
         }
     }
 }
 
-initialize(16);
+initialize();
