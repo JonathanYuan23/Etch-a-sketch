e0789aa89141db2320751d6fd797133327a1f669
Jonathan Yuan <jy90023@gmail.com>
2021-07-30 16:44:56 -0400

    finished drawing on sketch pad during different modes functionality

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -3,19 +3,42 @@ const resetBtn = document.querySelector('#resetBtn');
 const clearBtn = document.querySelector('#clearBtn');
 const eraseBtn = document.querySelector('#eraseBtn');
 const colorBtn = document.querySelector('#colorBtn');
-const randomBtn = document.querySelector('randomBtn');
+const randomBtn = document.querySelector('#randomBtn');
 
 const color = 'rgb(50, 50, 50)';
-const resetColor = 'rgb(255, 255, 255)';
+const resetColor = 'rgb(250, 250, 250)';
 
 let mode = 'color';
 let click = 0;
 
-function randomRGB() {}
+function random() {
+    return Math.floor(Math.random() * 256);
+}
 
-function changeColor(e) {}
+function randomRGB() {
+    return `rgb(${random()}, ${random()}, ${random()})`;
+}
+
+function changeColor(e) {
+    if (e.type === 'mouseover' && !click) return;
+    if (e.type === 'click') click = !click;
+
+    switch (mode) {
+        case 'color':
+            e.target.style.backgroundColor = color;
+            break;
+        case 'erase':
+            e.target.style.backgroundColor = resetColor;
+            break;
+        case 'random':
+            e.target.style.backgroundColor = randomRGB();
+    }
+}
 
 function initialize(width) {
+    (sketchPad.innerHTML = ''), (click = 0);
+    sketchPad.style.gridTemplateRows = `repeat(${width}, 1fr)`;
+    sketchPad.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
     for (let row = 1; row <= width; row++) {
         for (let col = 1; col <= width; col++) {
             const item = document.createElement('div');
@@ -23,11 +46,9 @@ function initialize(width) {
             /*
             for future use if I ever want to add more complex functionality involving opacity
 
-            ================================
             item.style.gridRow = `${row}`;
             item.style.gridColumn = `${col}`;
             item.classList.add('100');
-            ================================
             */
 
             item.addEventListener('click', changeColor);
@@ -47,13 +68,20 @@ function initializeOptions() {
         else initialize(newWidth);
     });
     clearBtn.addEventListener('click', () => {
+        click = 0;
         sketchPad.childNodes.forEach(function (e) {
             e.style.backgroundColor = resetColor;
         });
     });
-    eraseBtn.addEventListener('click', () => (mode = 'erase'));
-    colorBtn.addEventListener('click', () => (mode = 'color'));
-    randomBtn.addEventListener('click', () => (mode = 'random'));
+    eraseBtn.addEventListener('click', () => {
+        (mode = 'erase'), (click = 0);
+    });
+    colorBtn.addEventListener('click', () => {
+        (mode = 'color'), (click = 0);
+    });
+    randomBtn.addEventListener('click', () => {
+        (mode = 'random'), (click = 0);
+    });
 }
 
 initialize(16);
